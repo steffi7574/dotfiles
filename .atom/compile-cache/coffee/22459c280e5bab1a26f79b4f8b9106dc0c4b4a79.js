636c6cc031755fda1e41b00553b4622e155e0c34
Stefanie Guenther <guenther@mathcces.rwth-aachen.de>
2016-03-30 21:32:45 +0200

    Delete unnecessary atom files

--- .atom/compile-cache/coffee/22459c280e5bab1a26f79b4f8b9106dc0c4b4a79.js
+++
@@ -1,8 +0,0 @@
-(function() {
-
-
-}).call(this);
-
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiL2hvbWUvc2d1ZW50aGVyLy5hdG9tL3BhY2thZ2VzL3ZpbS1tb2RlLXBsdXMvc3BlYy9ob3Zlci1zcGVjLmNvZmZlZSIKICBdLAogICJuYW1lcyI6IFtdLAogICJtYXBwaW5ncyI6ICJBQThCMkI7QUFBQTtBQUFBIgp9
-
-//# sourceURL=/home/sguenther/.atom/packages/vim-mode-plus/spec/hover-spec.coffee
