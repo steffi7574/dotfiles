bb6af2640ee8336ab506ab30bdc9dcae6a6413b0
Stefanie Guenther <guenther@mathcces.rwth-aachen.de>
2018-07-03 14:34:17 +0200

    Set PATH in fishconfig

--- .fishconfig/config.fish
+++ .fishconfig/config.fish
@@ -1,5 +1,10 @@
-set -x PATH $PATH /home/sguenther/Numerics/SU2/bin
+set -x SU2_RUN /home/sguenther/Numerics/SU2/bin
+set -x ICECREAM_PATH /usr/libexec/icecc/bin
+set -x OPENMPI_PATH  /usr/lib64/openmpi/bin
+set -x OPENMPI_LD_PATH /usr/lib64/openmpi/lib
+
+set -x PATH $SU2_RUN $ICECREAM_PATH $OPENMPI_PATH $PATH
+set -x LD_LIBRARY_PATH $OPENMPI_LD_PATH $LD_LIBRARY_PATH
 
 set -x ICECC_VERSION /home/sguenther/Software/gcc7.3.1.tar.gz
 set -x ICECC_CARET_WORKAROUND 0
-set -x PATH $PATH /usr/libexec/icecc/bin
