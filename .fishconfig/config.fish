747ec7e1026b3dd01371d64e876f245b91fb0b45
Stefanie Guenther <guenther@mathcces.rwth-aachen.de>
2018-08-02 15:34:50 +0200

    Add pythonutils to pythonpath in fishconfig.

--- .fishconfig/config.fish
+++ .fishconfig/config.fish
@@ -5,6 +5,7 @@ set -x OPENMPI_LD_PATH /usr/lib64/openmpi/lib
 
 set -x PATH $SU2_RUN $ICECREAM_PATH $OPENMPI_PATH $PATH
 set -x LD_LIBRARY_PATH $OPENMPI_LD_PATH $LD_LIBRARY_PATH
+set -x PYTHONPATH /home/sguenther/Numerics/pythonscripting
 
 set -x ICECC_VERSION /home/sguenther/Software/gcc7.3.1.tar.gz
 set -x ICECC_CARET_WORKAROUND 0
