5649b649aebdb0f7e57bf0312a9f170fd54d505e
Stefanie Guenther <guenther5@llnl.gov>
2019-11-13 15:09:44 -0800

    Cleanup fishconfig.

--- .fishconfig/config.fish
+++ .fishconfig/config.fish
@@ -1,11 +1,2 @@
-set -x SU2_RUN /home/sguenther/Numerics/SU2/bin
-set -x ICECREAM_PATH /usr/libexec/icecc/bin
-set -x OPENMPI_PATH  /usr/lib64/openmpi/bin
-set -x OPENMPI_LD_PATH /usr/lib64/openmpi/lib
-
-set -x PATH $SU2_RUN $ICECREAM_PATH $OPENMPI_PATH $PATH
-set -x LD_LIBRARY_PATH $OPENMPI_LD_PATH $LD_LIBRARY_PATH
-set -x PYTHONPATH /home/sguenther/Numerics/DNN_PinT/pythonutil
-
-set -x ICECC_VERSION /home/sguenther/Software/gcc7.3.1.tar.gz
-set -x ICECC_CARET_WORKAROUND 0
+set -x MELD_DIR $HOME/Software/meld-3.20.1/bin
+set -x PATH     $MELD_DIR $PATH
